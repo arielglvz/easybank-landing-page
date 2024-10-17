29a8eb4b040ce78bc51c9463577223e3e13a3df9
yeyel <arielglvz@gmail.com>
2024-10-17 19:52:34 +0800

    Animated Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,29 @@
 const btnHamBurger = document.querySelector('#btnHamBurger');
+const body = document.querySelector('body');
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamBurger.addEventListener('click', function() {
   console.log('open Hamburger');
   
   if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
     header.classList.remove('open');
-    overlay.classList.remove('fade-in');
-    overlay.classList.add('fade-out');
+    fadeElems.forEach(function(element) {
+      console.log(`e open: ${header.classList.contains('open')}`, element)
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    })
+    
   } 
   else { // Open Hamburger Menu
+    body.classList.add('noscroll');
     header.classList.add('open');
-    overlay.classList.remove('fade-out');
-    overlay.classList.add('fade-in');
+    fadeElems.forEach(function(element) {
+      console.log(`e open: ${header.classList.contains('open')}`, element)
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    })
   }
 })
